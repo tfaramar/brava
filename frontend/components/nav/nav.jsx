5c28fedc48483db274ed9aa787a6f98adf24270c
tfaramarzi <tfaramarzi@gmail.com>
2020-01-08 16:12:33 -0800

    Update nav component to properly toggle session buttons

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = ({ currentUser, logout }) => {
+    
+    const [formType, setFormType] = useState(window.location.hash);
+
+    window.addEventListener('hashchange', () => {
+        setFormType(window.location.hash);
+    }, false);
 
     let sessionButton;
-    if (window.location.hash === "#/login") {
+    if (formType === "#/login") {
         sessionButton = <Link to="/signup"><button className="nav-signup">Sign up</button></Link>
-    } else if (window.location.hash === "#/signup" || window.location.hash === "#/" && !currentUser) {
+    } else if (formType === "#/signup" || formType === "#/" && !currentUser) {
         sessionButton = <Link to="/login"><button className="nav-login">Login</button></Link>
     };
 
@@ -16,14 +22,15 @@ const Nav = ({ currentUser, logout }) => {
         </ul>
     );
 
-    const personalNav = () => (
-        <hgroup className="header-group">
-            <h2 className="header-name">Hi, {currentUser.firstName}!</h2>
-            <button className="header-button" onClick={logout}>Log Out</button>
-        </hgroup>
-    );
+    // const personalNav = () => (
+    //     <hgroup className="header-group">
+    //         <h2 className="header-name">Hi, {currentUser.firstName}!</h2>
+    //         <button className="header-button" onClick={logout}>Log Out</button>
+    //     </hgroup>
+    // );
 
     //return currentUser ? personalNav() : sessionLinks();
+    
     return (
         <div className="nav-bar-container">
             <Link to="/" className="header-link">
