50868a1b6da947497067e2a648512ddf2d9c3495
tfaramarzi <tfaramarzi@gmail.com>
2020-01-14 12:22:30 -0800

    Update activity seeds and add demo avatar image

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,6 +54,9 @@ ActiveRecord::Base.transaction do
         user_id: 1,
         sport: 1,
         title: 'How to beat the flu',
+        distance: 40.73,
+        elevation: 6188,
+        elapsed_time: 15932,
         personal_record: false,
         start_time: Time.zone.at(1449506825)
     )
@@ -62,6 +65,9 @@ ActiveRecord::Base.transaction do
         user_id: 1,
         sport: 2,
         title: 'Type 2 fun',
+        distance: 5.76,
+        elevation: 598,
+        elapsed_time: 4200,
         personal_record: true,
         start_time: Time.zone.at(1449506820)
     )
@@ -70,6 +76,9 @@ ActiveRecord::Base.transaction do
         user_id: 2,
         sport: 2,
         title: 'Nothing can be changed until it is faced!',
+        distance: 8.19,
+        elevation: 208,
+        elapsed_time: 4118,
         personal_record: true,
         start_time: Time.zone.at(1449506820)
     )
@@ -78,6 +87,9 @@ ActiveRecord::Base.transaction do
         user_id: 3,
         sport: 1,
         title: 'I\'m unstoppable on my bicycle',
+        distance: 22.37,
+        elevation: 1229,
+        elapsed_time: 4450,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
@@ -86,6 +98,9 @@ ActiveRecord::Base.transaction do
         user_id: 3,
         sport: 1,
         title: 'Freedom in the headlands',
+        distance: 8.19,
+        elevation: 208,
+        elapsed_time: 4118,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
@@ -94,6 +109,9 @@ ActiveRecord::Base.transaction do
         user_id: 1,
         sport: 1,
         title: 'Afternoon Ride',
+        distance: 15.54,
+        elevation: 1571,
+        elapsed_time: 4665,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
@@ -102,6 +120,9 @@ ActiveRecord::Base.transaction do
         user_id: 3,
         sport: 2,
         title: 'Afternoon run',
+        distance: 4.70,
+        elevation: 118,
+        elapsed_time: 2018,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
@@ -109,14 +130,17 @@ ActiveRecord::Base.transaction do
     act8 = Activity.create!(
         user_id: 2,
         sport: 2,
-        title: 'Run on Beale St',
+        title: 'Walking on Beale St',
+        distance: 1.00,
+        elevation: 264,
+        elapsed_time: 2100,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
 
     act9 = Activity.create!(
         user_id: 4,
-        sport: 2,
+        sport: 1,
         title: 'Riding out that hangover',
         distance: 2.45,
         elevation: 478,
@@ -127,57 +151,78 @@ ActiveRecord::Base.transaction do
 
     act10 = Activity.create!(
         user_id: 1,
-        sport: 2,
-        title: 'Bike camping forever.',
-        personal_record: false,
-        start_time: Time.zone.at(1449506820)
-    )
-
-    act10 = Activity.create!(
-        user_id: 3,
         sport: 1,
-        title: 'I do my hair toss',
-        personal_record: true,
+        title: 'Bike camping forever.',
+        distance: 22.80,
+        elevation: 2058,
+        elapsed_time: 6780,
+        personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
 
     act11 = Activity.create!(
         user_id: 1,
-        sport: 2,
+        sport: 1,
         title: 'Presidio ridez',
+        distance: 23.88,
+        elevation: 3222,
+        elapsed_time: 8160,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
 
     act12 = Activity.create!(
         user_id: 4,
-        sport: 1,
+        sport: 2,
         title: 'I was walking.',
+        distance: 1.37,
+        elevation: 348,
+        elapsed_time: 4160,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
 
     act13 = Activity.create!(
         user_id: 1,
-        sport: 1,
+        sport: 2,
         title: 'Morning run',
+        distance: 2.45,
+        elevation: 160,
+        elapsed_time: 5160,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
 
     act14 = Activity.create!(
         user_id: 3,
-        sport: 1,
+        sport: 2,
         title: 'Golden gate park ramble',
+        distance: 3.37,
+        elevation: 188,
+        elapsed_time: 9160,
         personal_record: false,
         start_time: Time.zone.at(1449506820)
     )
 
     act15 = Activity.create!(
         user_id: 2,
-        sport: 2,
+        sport: 1,
         title: 'Sunset ride',
+        distance: 11.98,
+        elevation: 1200,
+        elapsed_time: 5100,
         personal_record: false,
+        start_time: Time.zone.at(1449506820)
+    )
+
+    act16 = Activity.create!(
+        user_id: 3,
+        sport: 2,
+        title: 'I do my hair toss',
+        distance: 9.21,
+        elevation: 1345,
+        elapsed_time: 6160,
+        personal_record: true,
         start_time: Time.zone.at(1449506820)
     )
 
