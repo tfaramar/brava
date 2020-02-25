ecd504b450c5363cd0575bd6f1374fbaed5947e9
tfaramarzi <tfaramarzi@gmail.com>
2020-02-25 14:19:11 -0800

    Refine navbar dropdown styles

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -9,10 +9,6 @@ const Nav = ({ currentUser, logout, removeErrors }) => {
         setFormType(window.location.hash);
     }, false);
 
-    const toggleDropdown = (id) => {
-        document.getElementById(id).classList.toggle("show");
-    }
-
     let sessionButton;
     if (formType === "#/login") {
         sessionButton = <Link to="/signup"><button onClick={() => removeErrors()} className="nav-signup">Sign up</button></Link>
@@ -44,14 +40,26 @@ const Nav = ({ currentUser, logout, removeErrors }) => {
                 </div>
             </div>
             <div className="right-group">
-                <div id="nav-image-dropdown">
-                    <div className="nav-image-wrapper">
-                        <img className="nav-image" src={window.demo_avatar_imgURL} alt="Your profile photo" />
+                <div className="nav-image-dropdown">
+                    <button type="button" className="nav-image-toggle">
+                        <div className="nav-image-wrapper">
+                            <img className="nav-image" src={window.demo_avatar_imgURL} alt="Your profile photo" />
+                        </div>
+                        <i className="fas fa-angle-down"></i>
+                    </button>
+                    <div className="ni-dropdown-content">
+                        <p>My Profile</p>
+                        <p>Logout</p>
                     </div>
-                    <i className="fas fa-angle-down"></i>
                 </div>
-                <div id="nav-circle-dropdown">
-                    <div className="nav-circle"><i className="fas fa-plus-circle"></i></div>
+                <div className="nav-circle-dropdown">
+                    <button type="button" className="nav-circle-toggle">
+                        <div className="nav-circle"><i className="fas fa-plus-circle"></i></div>
+                    </button>
+                    <div className="cir-dropdown-content">
+                        <p>Add Activity</p>
+                        <p>Create a Route</p>
+                    </div>
                 </div>
                 
             </div>
