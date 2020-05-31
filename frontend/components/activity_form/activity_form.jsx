019df06db0d8ead767d57f362d2d7df00407ac43
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-05-31 16:43:16 -0700

    Add input validations and fix datepicker

--- frontend/components/activity_form/activity_form.jsx
+++ frontend/components/activity_form/activity_form.jsx
@@ -1,47 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 
-import { camelToSnakeCase } from '../../util/helper_functions';
+import { camelToSnakeCase, unformatDuration } from '../../util/helper_functions';
 
 const ActivityForm = (props) => {
     const [activityData, setActivityData] = useState({
         sport: 1,
         title: "Afternoon run",
-        distance: 21.73,
-        elevation: 618,
-        hours: "test",
-        mins: "text",
-        secs: "text",
+        distance: 0,
+        elevation: 0,
+        hours: "",
+        mins: "",
+        secs: "",
         startDate: new Date(),
         routeId: null
     });
-
-    //handle numeric text entry separately
 
     const handleChange = e => {
         setActivityData({...activityData, [e.target.name]: e.target.value});
     };
 
+    const handleTimeChange = e => {
+        let num = parseInt(e.target.value);
+        if (num > 59) {
+            //set error on input
+        } else {
+            setActivityData({ ...activityData, [e.target.name]: num });
+        }  
+    }
+
+    const handleFloatChange = e => {
+        setActivityData({ ...activityData, [e.target.name]: parseFloat(e.target.value) });
+    };
+
+    const handleSportChange = e => {
+        let num = parseInt(e.target.value);
+        setActivityData({ ...activityData, [e.target.name]: num });
+    };
+
     const handleSubmit = e => {
         e.preventDefault();
         console.log(activityData);
+        // unformatDuration(activityData.hours, activityData.mins, activityData.secs)
     };
 
     return (
         <div className="activity-form-container">
             <h1 className="activity-form-title">Manual Activity Entry</h1>
             <div>
-                <form className="activity-form">
+                <form onSubmit={handleSubmit} className="activity-form">
                     <div className="upper-form">
                         <div className="fieldset">
                             <label> Distance </label>
-                            <input 
-                                type="number" 
-                                value={activityData.distance}
-                                name="distance"
-                                onChange={handleChange}
-                            /> miles
-                        
+                            <div className="compound-input">
+                                <input
+                                    type="number"
+                                    min="0"
+                                    value={activityData.distance}
+                                    name="distance"
+                                    onChange={handleFloatChange}
+                                /> miles
+                            </div>
                         </div>
                         <div className="fieldset">
                             <label> Duration </label>
@@ -49,6 +68,7 @@ const ActivityForm = (props) => {
                                 <input
                                     type="number"
                                     placeholder="00 hours"
+                                    min="0"
                                     value={activityData.hours}
                                     name="hours"
                                     onChange={handleChange}
@@ -56,6 +76,8 @@ const ActivityForm = (props) => {
                                 <input
                                     type="number"
                                     placeholder="00 minutes"
+                                    min="0"
+                                    max="59"
                                     value={activityData.mins}
                                     name="mins"
                                     onChange={handleChange}
@@ -63,6 +85,8 @@ const ActivityForm = (props) => {
                                 <input
                                     type="number"
                                     placeholder="00 seconds"
+                                    min="0"
+                                    max="59"
                                     value={activityData.secs}
                                     name="secs"
                                     onChange={handleChange}
@@ -76,44 +100,46 @@ const ActivityForm = (props) => {
                                     type="number"
                                     value={activityData.elevation}
                                     name="elevation"
-                                    onChange={handleChange}
+                                    onChange={handleFloatChange}
                                 /> feet
                             </div>
                         </div>
                     </div>
-                    <div className="lower-form">
-                        <div className="fieldset">
-                            <label> Sport </label>
-                            <select name="sport">
-                                <option value="1">Ride</option>
-                                <option value="2">Run</option>
-                            </select>
+                    <div className="lower-metaform">
+                        <div className="lower-form">
+                            <div className="fieldset">
+                                <label> Sport </label>
+                                <select className="custom-select" name="sport" onChange={handleSportChange}>
+                                    <option value="1">Ride</option>
+                                    <option value="2">Run</option>
+                                </select>
+                                 
+                            </div>
+                            <div className="fieldset">
+                                <label> Date &amp; Time </label>
+                                <DatePicker
+                                    selected={activityData.startDate}
+                                    onChange={date => setActivityData({ ...activityData, startDate: date })}
+                                    showTimeSelect
+                                    timeFormat="HH:mm"
+                                    timeIntervals={15}
+                                    timeCaption="time"
+                                    dateFormat="MMMM d, yyyy h:mm aa"
+                                />
+                            </div>
                         </div>
                         <div className="fieldset">
-                            <label> Date / Time </label>
-                            <DatePicker
-                                selected={activityData.startDate}
-                                onChange={date => setActivityData({ ...activityData, startDate: date })}
-                                showTimeSelect
-                                timeFormat="HH:mm"
-                                timeIntervals={10}
-                                timeCaption="time"
-                                dateFormat="MMMM d, yyyy h:mm aa"
-                            />
-                        </div>
-                        <div className="fieldset">
-                            <label> Title
+                            <label> Title </label>
                                 <input 
                                     type="text" 
                                     value={activityData.title}
                                     name="title"
                                     onChange={handleChange} 
                                 />
-                            </label>
                         </div>
                     </div>  
                     <div className="route-content">
-                        Routes to be shown here based on the sport type
+                        BUNCH OF ROUTES TO CHOOSE FROM BASED ON SPORT TYPE
                     </div> 
                     <div className="activity-buttons">
                         <button className="submit-activity-button" type="submit">Create</button>
