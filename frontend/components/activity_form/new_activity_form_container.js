05b3d1b5fad52e5a8c81ef79c03fc4ea99230c6c
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-06-04 22:15:32 -0700

    Update activity form container

--- frontend/components/activity_form/new_activity_form_container.js
+++ frontend/components/activity_form/new_activity_form_container.js
@@ -1,17 +1,22 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import {  createActivity, removeErrors } from '../../actions/activity_actions';
+import { fetchRoutes } from '../../actions/route_actions';
+import { selectRoutes } from '../../reducers/selectors';
 import ActivityForm from './activity_form';
 
-const mapStateToProps = ({ errors }) => ({
-    errors: errors.activities,
+const mapStateToProps = state => ({
+    errors: state.errors.activities,
     formType: 'Create',
-    message: 'Manual Entry'
+    message: 'Manual Entry',
+    routes: selectRoutes(state)
 });
 
 const mapDispatchToProps = dispatch => ({
+    fetchRoutes: () => dispatch(fetchRoutes()),
     processForm: (activity) => dispatch(createActivity(activity)),
     removeErrors: () => removeErrors(dispatch)
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ActivityForm);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ActivityForm));
