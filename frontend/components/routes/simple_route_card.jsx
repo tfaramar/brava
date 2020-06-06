a9347a5530a234a833ac6becc78c6de1994598e1
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-06-05 21:53:32 -0700

    Build simple route card component

---
+++ frontend/components/routes/simple_route_card.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import RouteMap from './route_map';
+import { formatDate, formatTime } from '../../util/helper_functions';
+
+const SimpleRouteCard = ({ route, idx, selectRoute }) => {
+    return (
+        <div onClick={() => selectRoute(idx)} className="simple-route-card">
+            <div className="route-map-embed">
+                <RouteMap coordinates={route.coordinates} container={`map-route-${route.id}`} />
+            </div>
+            <div className="route-content">
+                <h2 className="route-title">{route.title}</h2>
+                <ul className="list-stats">
+                    <li className="stat">
+                        <div className="stat-subtext">Distance</div>
+                        <div className="stat-text">{route.distance} mi</div>
+                    </li>
+                    <li className="stat">
+                        <div className="stat-subtext">Est. Time</div>
+                        <div className="stat-text">{formatTime(route.time)}</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default SimpleRouteCard;
